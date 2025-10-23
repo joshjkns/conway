ff1af36a3d3a839acf54bd6af5dcd6886626713c
Jack <wm24026@bristol.ac.uk>
2025-10-23 13:07:47 +0100

    passes tests until 3, maybe buttosn work?

--- gol/distributor.go
+++ gol/distributor.go
@@ -57,7 +57,6 @@ func getAliveCells(world *[][]byte, p *Params) []util.Cell {
 func pgmImage(p *Params, world *[][]byte, c *distributorChannels, turns *int) {
 	(*c).ioCommand <- ioOutput
 	filename := strconv.Itoa((*p).ImageWidth) + "x" + strconv.Itoa((*p).ImageHeight) + "x" + strconv.Itoa(*turns)
-	fmt.Println(filename)
 	(*c).ioFilename <- filename
 
 	for y := 0; y < (*p).ImageHeight; y++ {
@@ -105,79 +104,87 @@ func distributor(p Params, c distributorChannels) {
 
 	ticker := time.NewTicker(2 * time.Second)
 	defer ticker.Stop()
+	done := make(chan *rpc.Call, 1)
+	go client.Go("DistributingComp.Process", args, &reply, done)
 
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				var empty Input
-				var alive Output
+	for {
+		select {
+		case <-ticker.C:
+			var empty Input
+			var alive Output
+			if !paused {
 				err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
 				if err != nil {
 					panic(err)
 				}
 				aliveCells := getAliveCells(&alive.World, &p)
 				c.events <- AliveCellsCount{CompletedTurns: alive.Turns, CellsCount: len(aliveCells)}
-			case kp := <-c.keyPresses:
-				switch kp {
-				case 'q':
-					err = client.Call("DistributingComp.Unregister", id, nil)
-					if err != nil {
-						panic(err)
-					}
-					return // returing out of the go func FIX
-				case 's':
-					var empty Input
-					var alive Output
-					err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
-					if err != nil {
-						panic(err)
-					}
-					pgmImage(&p, &alive.World, &c, &alive.Turns)
-				case 'k':
-					var empty Input
-					var alive Output
-					err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
-					if err != nil {
-						panic(err)
-					}
-					c.events <- FinalTurnComplete{CompletedTurns: alive.Turns, Alive: getAliveCells(&alive.World, &p)}
-					pgmImage(&p, &alive.World, &c, &alive.Turns)
-					c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Quitting}
-					_ = client.Call("DistributingComp.QuitProgram", nil, nil)
-					return
-				case 'p':
-					var alive Output
-					if paused {
-						_ = client.Call("DistributingComp.TogglePaused", nil, nil)
-						err = client.Call("DistributingComp.GetCurrentState", nil, &alive)
-						c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Executing}
-						paused = false
-					} else {
-						err = client.Call("DistributingComp.GetCurrentState", nil, &alive)
-						_ = client.Call("DistributingComp.TogglePaused", nil, nil)
-						c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Paused}
-						fmt.Println(alive.Turns)
-						paused = true
-					}
+			}
+		case kp := <-c.keyPresses:
+			switch kp {
+			case 'q':
+				var empty Input
+				var alive Output
+				err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
+				err = client.Call("DistributingComp.Unregister", id, nil)
+				if err != nil {
+					panic(err)
+				}
+				c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Quitting}
+				return
+			case 's':
+				var empty Input
+				var alive Output
+				err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
+				if err != nil {
+					panic(err)
+				}
+				pgmImage(&p, &alive.World, &c, &alive.Turns)
+			case 'k':
+				var empty Input
+				var alive Output
+				err := client.Call("DistributingComp.GetCurrentState", empty, &alive)
+				if err != nil {
+					panic(err)
+				}
+				c.events <- FinalTurnComplete{CompletedTurns: alive.Turns, Alive: getAliveCells(&alive.World, &p)}
+				pgmImage(&p, &alive.World, &c, &alive.Turns)
+				c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Quitting}
+				_ = client.Call("DistributingComp.QuitProgram", empty, &alive)
+				return
+			case 'p':
+				var empty Input
+				var alive Output
+
+				if paused {
+					err = client.Call("DistributingComp.GetCurrentState", empty, &alive)
+					_ = client.Call("DistributingComp.TogglePaused", false, &alive)
+					c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Executing}
+					paused = false
+				} else {
+					err = client.Call("DistributingComp.TogglePaused", true, &alive)
+					err = client.Call("DistributingComp.GetCurrentState", empty, &alive)
+
+					c.events <- StateChange{CompletedTurns: alive.Turns, NewState: Paused}
+					fmt.Println(alive.Turns)
+					paused = true
 				}
 			}
+		case <-done:
+			if err != nil {
+				panic(err)
+			}
+
+			c.events <- FinalTurnComplete{CompletedTurns: reply.Turns, Alive: getAliveCells(&reply.World, &p)}
+
+			pgmImage(&p, &reply.World, &c, &reply.Turns)
+
+			c.ioCommand <- ioCheckIdle
+			<-c.ioIdle
+			c.events <- StateChange{reply.Turns, Quitting}
+
+			close(c.events)
+			return
 		}
-	}()
-
-	err = client.Call("DistributingComp.Process", args, &reply)
-	if err != nil {
-		panic(err)
 	}
-
-	c.events <- FinalTurnComplete{CompletedTurns: reply.Turns, Alive: getAliveCells(&reply.World, &p)}
-
-	pgmImage(&p, &reply.World, &c, &reply.Turns)
-
-	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
-
-	c.events <- StateChange{reply.Turns, Quitting}
-
-	close(c.events)
 }
